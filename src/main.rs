9dbb7a091ba625c701c08e6d8e0e7602009a89e1
togatoga <togasakitogatoga@gmail.com>
2019-12-07 12:22:02 +0900

    Add parse_sample_cases

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,12 @@ extern crate clap;
 extern crate failure;
 extern crate reqwest;
 extern crate scraper;
+extern crate selectors;
 extern crate tokio;
 extern crate url;
 
+use itertools::Itertools;
+use selectors::Element;
 enum SubCommand {
     Download,
     Login,
@@ -35,10 +38,57 @@ async fn login(url: &str) -> Result<(), failure::Error> {
     Ok(())
 }
 
+fn parse_sample_cases(document: &scraper::Html) -> Result<Vec<(String, String)>, failure::Error> {
+    let task_statement_selector = scraper::Selector::parse(r#"div[id="task-statement"]"#).unwrap();
+    let pre_selector = scraper::Selector::parse("pre").unwrap();
+    let h3_selector = scraper::Selector::parse("h3").unwrap();
+    let input_h3_text = vec!["入力例", "Sample Input"];
+    let output_h3_text = vec!["出力例", "Sample Output"];
+
+    let mut input_cases = vec![];
+    let mut output_cases = vec![];
+    if let Some(task_statement) = document.select(&task_statement_selector).next() {
+        for pre in task_statement.select(&pre_selector) {
+            if let Some(pre_parent) = pre.parent_element() {
+                if let Some(h3) = pre_parent.select(&h3_selector).next() {
+                    let h3_text = h3.text().collect::<String>();
+                    let input = input_h3_text.iter().any(|&x| h3_text.contains(x));
+                    let output = output_h3_text.iter().any(|&x| h3_text.contains(x));
+                    let text = pre.text().collect::<String>();
+                    if input {
+                        input_cases.push(text);
+                    } else if output {
+                        output_cases.push(text);
+                    }
+                }
+            }
+        }
+    } else {
+        //return error
+    }
+    // make cases unique to remove extra duplicated language cases
+    let input_cases: Vec<String> = input_cases.into_iter().unique().collect();
+    let output_cases: Vec<String> = output_cases.into_iter().unique().collect();
+    println!("Found {} test cases", input_cases.len());
+    let sample_test_cases: Vec<(String, String)> = input_cases
+        .into_iter()
+        .zip(output_cases)
+        .enumerate()
+        .map(|(idx, (input, output))| {
+            println!("=== Sample Test Case {} ===", idx + 1);
+            println!("Input:\n{}\nOutput:\n{}", input, output);
+            println!("===========================");
+            (input, output)
+        })
+        .collect();
+    Ok(sample_test_cases)
+}
+
 async fn download(url: &str) -> Result<(), failure::Error> {
     let url = url::Url::parse(url)?;
     let html = get_html(url.as_str()).await?;
-    println!("{}", html);
+    let document = parse_html(&html);
+    let sample_test_cases = parse_sample_cases(&document)?;
     Ok(())
 }
 
